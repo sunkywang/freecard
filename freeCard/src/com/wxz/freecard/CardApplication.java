f56be677fbd529133e6d6aa110e9556187e92800
sunkywang <549419918@qq.com>
2015-04-07 17:28:22 +0800

    上传代码

---
+++ freeCard/src/com/wxz/freecard/CardApplication.java
@@ -0,0 +1,16 @@
+package com.wxz.freecard;
+
+import android.app.Application;
+
+public class CardApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+    
+    
+}
+
+
