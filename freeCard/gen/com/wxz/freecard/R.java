50ea8acb2cf685f32178f7d4cd5f18fa0d103dbd
sunkywang <549419918@qq.com>
2015-04-08 10:47:42 +0800

    删除bin

--- freeCard/gen/com/wxz/freecard/R.java
+++ freeCard/gen/com/wxz/freecard/R.java
@@ -53,9 +53,10 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_splash=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int main_fragment=0x7f030002;
-        public static final int main_title=0x7f030003;
+        public static final int ads_view_pager=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int main_fragment=0x7f030003;
+        public static final int main_title=0x7f030004;
     }
     public static final class menu {
         public static final int splash=0x7f090000;
